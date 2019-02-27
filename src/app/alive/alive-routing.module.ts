4f8fea0c9078550eb03f01a01404645d3e3e1564
krzvcho <krzysztof.piszczek@gmail.com>
2019-02-27 22:14:33 +0100

    ngrx begining

--- src/app/alive/alive-routing.module.ts
+++ src/app/alive/alive-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AliveMainComponent } from './alive-main/alive-main.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AliveMainComponent } from "./alive-main/alive-main.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: AliveMainComponent
   }
 ];
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AliveRoutingModule { }
+export class AliveRoutingModule {}
--- src/app/alive/alive.module.ts
+++ src/app/alive/alive.module.ts
@@ -1,25 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AliveRoutingModule } from './alive-routing.module';
-import { AliveMainComponent } from './alive-main/alive-main.component';
-import { SceneComponent } from './alive-main/scene/scene.component';
-import { LiveDotComponent } from './alive-main/scene/live-dot/live-dot.component';
+import { AliveRoutingModule } from "./alive-routing.module";
+import { AliveMainComponent } from "./alive-main/alive-main.component";
+import { SceneComponent } from "./alive-main/scene/scene.component";
+import { LiveDotComponent } from "./alive-main/scene/live-dot/live-dot.component";
 
 @NgModule({
-  declarations: [
-    AliveMainComponent,
-    SceneComponent,
-    LiveDotComponent
-  ],
-  imports: [
-    CommonModule,
-    AliveRoutingModule,
-  ],
+  declarations: [AliveMainComponent, SceneComponent, LiveDotComponent],
+  imports: [CommonModule, AliveRoutingModule],
   providers: [],
   bootstrap: [AliveMainComponent],
-  entryComponents: [
-    LiveDotComponent
-  ]
+  entryComponents: [LiveDotComponent]
 })
-export class AliveModule { }
+export class AliveModule {}
